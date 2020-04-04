7048909c07842b5871cd1e5f288a3ccdb0600186
ch98road <1027740945@qq.com>
2020-04-04 17:24:40 +0800

    20200404

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# leetcode
-Java算法记录
+# Leetcode
+
+- Java算法记录，将leetcode的题目按顺序实现
+
+- 源码均在SRC下命名格式：_xxxx.java     xxxx==题目编号
---
+++ src/cmcandy/solutions/_0001.java
@@ -0,0 +1,12 @@
+package cmcandy.solutions;
+
+/**
+ * Created by CHEN on 2020/4/3.
+ */
+public class _0001 {
+
+
+    public static void main(String[] args) {
+
+    }
+}
