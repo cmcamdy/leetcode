0af7bf964fe6a40136d88b6e5939c8d690aa79a1
ch98road <1027740945@qq.com>
2020-04-09 01:04:12 +0800

    0409

--- src/cmcandy/solutions/_0010.java
+++ src/cmcandy/solutions/_0010.java
@@ -5,64 +5,75 @@ package cmcandy.solutions;
  */
 public class _0010 {
     static class Solution {
-        public boolean isMatch(String s, String p) {
+        boolean isMatch(String s, String p) {
 
-            if (s == null && p == null) {
+            boolean s_empty = s.isEmpty( );
+            boolean p_empty = p.isEmpty( );
+
+            if (s_empty && p_empty) {
                 return true;
-            } else if (!(s != null && p != null)) {
+            } else if ((!s_empty && p_empty)) {
+
+                //两方有一方为空，则不匹配
                 return false;
-            }
-            char[] arr_s = s.toCharArray( );
-            char[] arr_p = p.toCharArray( );
-
-            int len_s = arr_s.length;
-            int len_p = arr_p.length;
-            int i = 0;
-            int j = 0;
-            boolean jud = true;
-
-
-            while (jud && i < len_p) {
-                if (arr_p[i] == '.' || arr_p[i] == arr_s[j]) {
-                    i++;
-                    j++;
-                } else if (arr_p[i] == '*') {
-                    if (arr_p[i - 1] == '.') {
-                        if (i == len_p - 1) {
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    }
-                    while (j < len_s && arr_s[j] == arr_p[i - 1]) {
-                        j++;
-                    }
-                    i++;
-                } else if (arr_p[i] != arr_s[j]) {
-                    if (j == 0) {
-                        i++;
-                    } else if (i < len_p - 1 && arr_p[i + 1] == '*') {
-                        i += 2;
-                    } else {
-                        jud = false;
-                    }
+            } else if ((s_empty && !p_empty)) {
+                if (p.length( ) == 1 || p.charAt(1) != '*') {
+                    return false;
+                } else if (p.length( ) > 1 && p.charAt(1) == '*') {
+                    return isMatch(s, p.substring(2));
                 }
-                if (i==len_p||j==len_s){
-                    jud =false;
-                }
-            }
-            if (i == len_p && j == len_s) {
-                return true;
             } else {
-                return false;
+                //两方都不为空
+
+                if ((p.charAt(0) == s.charAt(0) || p.charAt(0) == '.') && (p.length( ) == 1 || p.charAt(1) != '*')) {
+                    //考虑没有* 的情况 字母匹配成功
+                    return isMatch(s.substring(1), p.substring(1));
+
+                } else if (p.length( ) > 1) {
+                    if (p.charAt(0) != '.' && p.charAt(1) == '*') {
+                        //考虑有*的情况
+                        int indexs = 0;
+                        while (indexs < s.length( ) && s.charAt(indexs) == p.charAt(0)) {
+                            //考虑相等情况
+                            boolean jud = isMatch(s.substring(indexs), p.substring(2));
+                            if (jud) {
+                                return true;
+                            }
+                            indexs++;
+                        }
+                        return isMatch(s.substring(indexs), p.substring(2));
+                    } else if (p.charAt(0) == '.' && p.charAt(1) == '*') {
+                        //考虑 '.'+'*'的情况
+                        int indexs = 0;
+                        while (indexs < s.length( )) {
+                            //考虑相等情况
+                            boolean jud = isMatch(s.substring(indexs), p.substring(2));
+                            if (jud) {
+                                return true;
+                            }
+                            indexs++;
+                        }
+                        return isMatch(s.substring(indexs), p.substring(2));
+                    }
+                }
             }
+            return false;
         }
     }
+
 
     public static void main(String[] args) {
 //        "aaa"
 //        "a*a"
         Solution solution = new Solution( );
-        System.out.println(solution.isMatch("aa", "a*"));
+        System.out.println(solution.isMatch("mississippi", "mis*is*p*."));//f
+//        System.out.println(solution.isMatch("aab", "c*a*b"));
+        System.out.println(solution.isMatch("ab", ".*c"));//f
+        System.out.println(solution.isMatch("aaa", "ab*a*c*a"));//t
+        System.out.println(solution.isMatch("a", "ab*"));//t
+        System.out.println(solution.isMatch("bbbbba", ".*a*a"));//t
+        System.out.println(solution.isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*a*a*b"));//t
+        //"mississippi"
+//        "mis*is*p*."
     }
 }
---
+++ src/cmcandy/solutions/_0011.java
@@ -0,0 +1,11 @@
+package cmcandy.solutions;
+
+/**
+ * Created by CHEN on 2020/4/8.
+ */
+public class _0011 {
+
+
+
+
+}
