ea7e34b25ce2209a165c3011da1703201702b0cc
ch98road <1027740945@qq.com>
2020-05-06 21:58:40 +0800

    '05062158'

--- src/cmcandy/solutions/_0026.java
+++ src/cmcandy/solutions/_0026.java
@@ -4,4 +4,22 @@ package cmcandy.solutions;
  * Created by CHEN on 2020/5/6.
  */
 public class _0026 {
+
+    static class Solution {
+        public int removeDuplicates(int[] nums) {
+            int index = 0;
+            for(int i = 1 ; i < nums.length ; i++){
+                if(nums[index]!=nums[i]){
+                    index++;
+                    nums[index] = nums[i];
+                }
+            }
+            return index+1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.removeDuplicates(new int[]{1,1,2}) );
+    }
 }
--- src/cmcandy/solutions/_0027.java
+++ src/cmcandy/solutions/_0027.java
@@ -4,4 +4,23 @@ package cmcandy.solutions;
  * Created by CHEN on 2020/5/6.
  */
 public class _0027 {
+    static class Solution {
+        public int removeElement(int[] nums, int val) {
+            int tmp = nums[0];
+            int index = 0;
+            for(int i = 1;i<nums.length;i++){
+                if(nums[i] != val){
+                    nums[index++]=nums[i];
+                }
+            }
+            nums[index] = tmp;
+            return tmp==val?index:index+1;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution  = new Solution();
+        System.out.println(solution.removeElement(new int[]{0,1,2,2,3,0,4,2},2) );
+    }
 }
