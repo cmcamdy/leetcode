2e5549d63a3f815dd5e30039026355c4d7949dcf
ch98road <1027740945@qq.com>
2020-05-14 14:49:05 +0800

    05141449

---
+++ src/cmcandy/solutions/_0046.java
@@ -0,0 +1,17 @@
+package cmcandy.solutions;
+
+import java.util.List;
+
+/**
+ * Created by CHEN on 2020/5/14.
+ */
+public class _0046 {
+
+    static class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+
+            return null;
+        }
+
+    }
+}
