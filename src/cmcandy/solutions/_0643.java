3ea86e0e3f6103e90aae1456e749816cdb9670d6
ch98road <1027740945@qq.com>
2020-05-22 17:18:31 +0800

    0522

---
+++ src/cmcandy/solutions/_0643.java
@@ -0,0 +1,28 @@
+package cmcandy.solutions;
+
+public class _0643 {
+    static class Solution {
+        public double findMaxAverage(int[] nums, int k) {
+    
+            //滑动窗口法
+            int len = nums.length;
+            if(len<1) return 0;
+            int sum = 0;
+            //init
+            for(int i =0 ;i<k;i++){
+                sum+=nums[i];
+            }
+            int max = sum;
+            for(int i = k;i<len;i++){
+                sum=sum-nums[i-k]+nums[i];
+                if(max<sum) max = sum;
+            }
+            return max*1.0/k;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findMaxAverage(new int[]{1,12,-5,-6,50,3}, 4));
+    }
+}
