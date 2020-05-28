e9239a979a2827355bf0c8d99146a4e857e4ed57
ch98road <1027740945@qq.com>
2020-05-28 20:19:07 +0800

    0528

---
+++ src/cmcandy/C_Language_Answers/_0083.c
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head){
+    if(head==NULL||head->next==NULL) return head;
+    struct ListNode *p = head;
+    while(p->next!=NULL){
+        if(p->next->val==p->val) p->next = p->next->next;
+        else p = p->next;
+    }
+    return head;
+}
