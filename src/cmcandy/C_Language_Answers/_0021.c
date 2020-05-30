bb25ecca208829b7fdf9a804023768ae9d3615af
ch98road <1027740945@qq.com>
2020-05-30 13:45:15 +0800

    0530

--- src/cmcandy/C_Language_Answers/_0021.c
+++ src/cmcandy/C_Language_Answers/_0021.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -5,41 +7,60 @@
  *     struct ListNode *next;
  * };
  */
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
 
+struct ListNode *mergeTwoLists(struct ListNode *l1, struct ListNode *l2)
+{
 
-struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
-    
-    if(l1==NULL) return l2;
-    else if(l2==NULL) return l1;
-    struct ListNode* p;
-    struct ListNode* head;
-    if(l1->val<=l2->val){
+    if (l1 == NULL)
+        return l2;
+    else if (l2 == NULL)
+        return l1;
+    struct ListNode *p;
+    struct ListNode *head;
+    if (l1->val <= l2->val)
+    {
         head = l1;
         l1 = l1->next;
-    } else{
+    }
+    else
+    {
         head = l2;
         l2 = l2->next;
     }
-    p  = head;
-    while(l1!=NULL||l2!=NULL){
-        if(l1==NULL) {
+    p = head;
+    while (l1 != NULL || l2 != NULL)
+    {
+        if (l1 == NULL)
+        {
             p->next = l2;
             return head;
-        }else if(l2 == NULL){
+        }
+        else if (l2 == NULL)
+        {
             p->next = l1;
             return head;
-        }else{
+        }
+        else
+        {
             //二者都不为空
 
-            if(l1->val<=l2->val){
-                    p->next = l1;
-                    l1 = l1->next;
-                    p = p->next;
-                } else{
-                    p->next = l2;
-                    l2 = l2->next;
-                    p = p ->next;
-                }
+            if (l1->val <= l2->val)
+            {
+                p->next = l1;
+                l1 = l1->next;
+                p = p->next;
+            }
+            else
+            {
+                p->next = l2;
+                l2 = l2->next;
+                p = p->next;
+            }
         }
     }
     return head;
