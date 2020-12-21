2d2caab1bed66125634f0680780ef3eecba49918
ch98road <49389600+ch98road@users.noreply.github.com>
2020-12-21 19:11:00 +0800

    20201221

---
+++ src/cmcandy/C_Language_Answers/_0746.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace ::std;
+class Solution
+{
+public:
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        //动规即可
+        int len = cost.size();
+        //基本情况，好像不用考虑也可以通过
+        // if(len==1) return 0;
+        // else if(len==2) return min(cost[0],cost[1]);
+        //填数组
+        for (int i = 2; i < len; i++)
+        {
+            cost[i] += min(cost[i - 1], cost[i - 2]);
+        }
+        //决定方案
+        return min(cost[len - 2], cost[len - 1]);
+    }
+};
