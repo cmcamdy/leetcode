bfd7ebb9b86aaaed8bb6fac2c0f730b080469bc1
ch98road <1027720945@qq.com>
2021-01-17 23:56:16 +0800

    20210117

---
+++ src/cmcandy/C_Language_Answers/_1232.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace ::std;
+class Solution
+{
+public:
+    bool checkStraightLine(vector<vector<int>> &coordinates)
+    {
+        //y=kx+b
+        int len = coordinates.size();
+        if (len <= 2)
+            return true;
+        //垂线
+        if (coordinates[1][0] == coordinates[0][0])
+        {
+            for (int i = 2; i < len; i++)
+            {
+                if (coordinates[0][0] != coordinates[i][0])
+                    return false;
+            }
+            return true;
+        }
+        else
+        {
+            //k=(y2-y1)/(x2-x1)
+            float k = 1.0 * (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
+            //带入得b
+            float b = coordinates[0][1] - k * coordinates[0][0];
+
+            for (int i = 2; i < len; i++)
+            {
+                if (coordinates[i][1] * 1.0 != (k * coordinates[i][0] + b))
+                    return false;
+            }
+            return true;
+        }
+    }
+};
