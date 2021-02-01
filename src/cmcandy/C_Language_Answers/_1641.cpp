38c132d45bd2955716346fa1c2718e46fdb0f473
ch98road <1027720945@qq.com>
2021-02-01 21:07:00 +0800

    20210201

---
+++ src/cmcandy/C_Language_Answers/_1641.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace ::std;
+class Solution
+{
+public:
+    int countVowelStrings(int n)
+    {
+        return (n + 4) * (n + 3) * (n + 2) * (n + 1) / 24;
+    }
+};
